c8530e8ec4fd34612ebe548e61ef2549b9b2ca68
Jered Wierzbicki <jeredw@gmail.com>
2024-01-01 22:37:04 -0800

    more day1

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ for January.
 [Black hole sun](https://jeredw.github.io/genuary2024/day1.html) type thing?  I
 could probably make many more particles if I moved the update loop out of JS...
 hmm.  Warning makes noise if you click.
+
+*Update*: I tried commenting out the `update()` calls and cranking particles up
+to a million and I only get 2-3 fps!  So canvas rasterizing is bottlenecking
+here.  Just switching to `putImageData` gets me up to 400-500k particles with
+comfortable fps.  Added some angular velocity and more general fooling around,
+this was super fun.
+
+*PS*: I commented out the sound because it was too annoying.
--- day1.js
+++ day1.js
@@ -1,23 +1,25 @@
-const N = 50000;
+const N = 400000;
 const WIDTH = 1000;
 const HEIGHT = 1000;
 const CURSOR_RADIUS = 50 * 50;
+const GROW = 10;
 const BOUNCE = false;
 const WRAP = false;
 const GRAVITY = 1e-7;
 const FRICTION = 0.95;
 
 class Particle {
-  constructor(x, y, vx, vy) {
+  constructor(x, y, vx, vy, wx) {
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
+    this.wx = wx;
     this.lastX = x;
     this.lastY = y;
   }
 
-  update(dt, mx, my, mb) {
+  update(dt, mx, my, mb, tmb) {
     this.lastX = this.x;
     this.lastY = this.y;
     const dx = this.x - mx;
@@ -28,13 +30,16 @@ class Particle {
       this.vx += -dx * dt * GRAVITY;
       this.vy += -dy * dt * GRAVITY;
       const d2 = dx * dx + dy * dy;
-      if (d2 < CURSOR_RADIUS) {
+      const grow = GROW * (tmb / 1000);
+      if (d2 < (CURSOR_RADIUS + grow * grow)) {
         const sign = mb ? -1 : 1;
         this.vx = sign * dx * 0.01;
         this.vy = sign * dy * 0.01;
       }
       this.vx *= FRICTION;
       this.vy *= FRICTION;
+      this.x += .1 * this.wx * -dy;
+      this.y += .1 * this.wx * dx;
     }
     if (Math.random() > 0.8) {
       this.vx += .1 * (0.1 - Math.random() * 0.2);
@@ -56,15 +61,24 @@ class Particle {
     }
   }
 
-  color() {
+  hue() {
     const d = this.vx * this.vx + this.vy * this.vy;
-    const hue = 360 * Math.sin(Math.PI * d);
-    return `hsla(${hue}, 75%, 50%, 0.5)`;
+    return 360 * Math.sin(Math.PI * d);
+  }
+
+  color() {
+    return `hsla(${this.hue()}, 75%, 50%, 0.5)`;
   }
 }
 
 class Simulation {
   constructor(canvas, ctx) {
+    this.firstUpdate = 0;
+    this.lastUpdate = 0;
+    this.lastReport = 0;
+    this.firstPressed = 0;
+    this.lastMouseButton = false;
+    this.frames = 0;
     this.particles = [];
     this.generateRandom();
     this.ctx = ctx;
@@ -84,7 +98,8 @@ class Simulation {
   }
 
   startAudio() {
-    if (this.audioCtx) return;
+    //if (this.audioCtx) return;
+    return;
     this.audioCtx = new AudioContext();
     this.osc1 = this.audioCtx.createOscillator();
     this.osc1.type = 'sine';
@@ -106,7 +121,8 @@ class Simulation {
       const y = (HEIGHT / 2) + r * Math.sin(theta);
       const vx = 0.1 - Math.random() * 0.2;
       const vy = 0.1 - Math.random() * 0.2;
-      this.particles[i] = new Particle(x, y, vx, vy);
+      const wx = Math.random() * 0.1;
+      this.particles[i] = new Particle(x, y, vx, vy, wx);
     }
   }
 
@@ -115,22 +131,43 @@ class Simulation {
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     let xs = 0;
     let xs2 = 0;
+    const imageData = this.ctx.createImageData(this.ctx.canvas.width, this.ctx.canvas.height);
+    if (!sim.mouseButton || !this.lastMouseButton) {
+      this.firstPressed = now;
+    }
     for (let i = 0; i < sim.particles.length; i++) {
       const p = sim.particles[i];
-      p.update(dt, sim.mouseX, sim.mouseY, sim.mouseButton);
-      this.ctx.beginPath();
-      this.ctx.fillStyle = p.color();
-      this.ctx.fillRect(p.x - 0.5, p.y - 0.5, 1, 1);
+      p.update(dt, sim.mouseX, sim.mouseY, sim.mouseButton, now - this.firstPressed);
+      //this.ctx.beginPath();
+      //this.ctx.fillStyle = p.color();
+      //this.ctx.fillRect(p.x - 0.5, p.y - 0.5, 1, 1);
+      if (0 <= p.x && p.x < 1000 && 0 <= p.y && p.y <= 1000) {
+        const color = hslToRgb(p.hue()/360, .75, .5);
+        const offset = 4 * imageData.width * (~~p.y) + 4 * (~~p.x);
+        imageData.data[offset] = color[0];
+        imageData.data[offset+1] = color[1];
+        imageData.data[offset+2] = color[2];
+        imageData.data[offset+3] = 128;
+      }
       xs += p.vx * p.vx + p.vy * p.vy;
       xs2 += Math.abs(p.x - sim.mouseX) + Math.abs(p.y - sim.mouseY);
     }
+    this.ctx.putImageData(imageData, 0, 0);
     if (this.audioCtx) {
       let freq1 = (xs / sim.particles.length) * 40000;
       this.osc1.frequency.setValueAtTime(freq1, this.audioCtx.currentTime);
       let freq2 = (xs2 / sim.particles.length) * 0.01;
       this.osc2.frequency.setValueAtTime(freq2, this.audioCtx.currentTime);
     }
+    this.lastMouseButton = sim.mouseButton;
     this.lastUpdate = now;
+    this.frames++;
+    if (!this.firstUpdate) {
+      this.firstUpdate = now;
+    } else if (now - this.lastReport >= 500) {
+      document.querySelector('.fps').innerHTML = Math.round(1000 * this.frames / (now - this.firstUpdate));
+      this.lastReport = now;
+    }
     requestAnimationFrame((now) => this.frame(now));
   }
 }
@@ -149,3 +186,29 @@ window.addEventListener('DOMContentLoaded', () => {
   sim = new Simulation(canvas, ctx);
   requestAnimationFrame((now) => sim.frame(now));
 });
+
+// https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+function hslToRgb(h, s, l) {
+  let r, g, b;
+
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hueToRgb(p, q, h + 1/3);
+    g = hueToRgb(p, q, h);
+    b = hueToRgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function hueToRgb(p, q, t) {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1/6) return p + (q - p) * 6 * t;
+  if (t < 1/2) return q;
+  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  return p;
+}
